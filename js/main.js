ff03e40df44d236867df13839a88cd9f83c884b0
Rogerio Junior <rogerio.junior1016@gmail.com>
2021-04-26 22:33:05 -0300

    Enigma 1

---
+++ js/main.js
@@ -0,0 +1,188 @@
+function mivsiw() {
+            var talvez=prompt("Digite sim")
+            if (talvez==="sim") {
+                window.location.replace('caesaris.html','pagename','resizable,height=260,width=370')
+            }
+            else {
+                alert("RESPOSTA ERRADA")
+            }
+        }
+
+function isdjinwd() {
+    var rolinh=prompt("")
+    if (rolinh==="dsoiwks") {
+        window.location.replace('fdsda.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+function dskdi() {
+    var sera=prompt("")
+    if (sera==="tomus i") {
+        window.location.replace('pertse.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+
+function fineihb2() {
+    var rolinha=prompt("")
+    if (rolinha==="UCSJYZPVAKXKNQ-HZYVHMACSA-N") {
+        window.location.replace('passado.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+
+function dsadq() {
+    var rolinh=prompt("")
+    if (rolinh==="dsoiwks") {
+        window.location.replace('dsg.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+function bgf24yt() {
+    var rolinh=prompt("")
+    if (rolinh==="dsoiwks") {
+        window.location.replace('g12c.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+function gsd1() {
+    var rolinh=prompt("")
+    if (rolinh==="dsoiwks") {
+        window.location.replace('fg21g.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+
+function dinwu2() {
+    var rolinhas=prompt("")
+    if (rolinhas==="senatus populusque romanus") {
+        window.location.replace('tabela.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+
+function botao() {
+    var rolinh=prompt("")
+    if (rolinh==="dsoiwks") {
+        window.location.replace('fin.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+
+function dslwok() {
+    var rolinh=prompt("")
+    if (rolinh==="14/06/2016") {
+        window.location.replace('maps.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+
+function klusi() {
+    var rolinh=prompt("")
+    if (rolinh==="pajac") {
+        window.location.replace('fin.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+
+function gq2f1() {
+    var rolinh=prompt("")
+    if (rolinh==="dsoiwks") {
+        window.location.replace('h311.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+
+function ge2wb() {
+    var rolinh=prompt("")
+    if (rolinh==="dsoiwks") {
+        window.location.replace('fasdwq.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+
+function g31g23() {
+    var rolinh=prompt("")
+    if (rolinh==="dsoiwks") {
+        window.location.replace('vsfkkkk.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+
+function g12g2d() {
+    var rolinh=prompt("")
+    if (rolinh==="dsoiwks") {
+        window.location.replace('g1123.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+
+function hhhf() {
+    var rolinh=prompt("")
+    if (rolinh==="dsoiwks") {
+        window.location.replace('fqsc.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+
+function dsd1f() {
+    var rolinh=prompt("")
+    if (rolinh==="dsoiwks") {
+        window.location.replace('g13fs.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+
+function g123() {
+    var rolinh=prompt("")
+    if (rolinh==="dsoiwks") {
+        window.location.replace('fsd1.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+
+function botao3() {
+    var rolinh=prompt("")
+    if (rolinh==="dsoiwks") {
+        window.location.replace('fin.html','pagename','resizable,height=260,width=370')
+    }
+    else {
+        alert("RESPOSTA ERRADA")
+    }
+}
+
+
